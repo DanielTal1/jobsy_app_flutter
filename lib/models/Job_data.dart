50c8cadbd60e7f8d4e1c9db664c727fc4444fb50
DanielTal1 <danieltal@outlook.com>
2023-05-05 21:50:56 +0300

    trying to add api calls for jobs list

--- lib/models/Job_data.dart
+++ lib/models/Job_data.dart
@@ -5,29 +5,35 @@ import 'package:flutter/foundation.dart';
 import 'job.dart';
 
 class JobData extends ChangeNotifier{
-  List<Job> jobsList=[
-    Job(company:"amazon",role:"Backend Software Engineer-DevEx Teamssss",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"1st interview"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"Offer"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"2nd interview"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"4th interview"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-    Job(company:"amazon",role:"developer",location:"Tel-aviv",addedTime:"25/3/2022",interview_stage:"applied"),
-    Job(company:"microsoft",role:"qa",location:"Tel-aviv", addedTime: "18/11/2023",interview_stage:"applied"),
-  ];
+  // List<Job> jobsList=[
+  //   Job(company:"amazon",role:"Backend Software Engineer-DevEx Teamssss",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"1st interview"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"Offer"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"2nd interview"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"4th interview"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  //   Job(company:"amazon",role:"developer",location:"Tel-aviv",lastUpdated:"25/3/2022",interview_stage:"applied"),
+  //   Job(company:"microsoft",role:"qa",location:"Tel-aviv", lastUpdated: "18/11/2023",interview_stage:"applied"),
+  // ];
+  List<Job> jobsList=[];
   late List<Job> JobsResult=jobsList;
   String currentQuery='';
 
 
 
+
+
+  void updateJobList(List<Job> newList){
+    jobsList=newList;
+  }
 
   void addJob(Job new_job){
     jobsList.insert(0,new_job);
--- lib/models/job.dart
+++ lib/models/job.dart
@@ -1,15 +1,34 @@
 
 class Job{
+  final String id;
   final String company;
   final String role;
   final String location;
   String? url;
   String interview_stage;
-  String? addedTime;
+  String? lastUpdated;
+  String? company_photo;
+  bool? archive;
+
+  factory Job.fromJson(Map<String,dynamic> parsedJson){
+    return Job(
+        id:parsedJson['_id'],
+        company:parsedJson['company'],
+        role:parsedJson['role'],
+        location:parsedJson['location'],
+        url:parsedJson['url'].toString(),
+        interview_stage:parsedJson['stage'],
+        lastUpdated:parsedJson['updatedAt'],
+        company_photo:parsedJson['company_photo'],
+        archive:parsedJson['archive']
+
+
+    );
+  }
 
 
 
-  Job({required this.company, required this.role, required this.location,this.addedTime,required this.interview_stage,this.url});
+  Job({required this.id,required this.company, required this.role, required this.location,this.lastUpdated,required this.interview_stage,this.url,this.company_photo,  this.archive});
 
   void changeStage(String new_stage ){
     this.interview_stage=new_stage;
